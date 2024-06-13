79f4da953cd0d79f752cd886c7bbf001cd5681e0
ANGJustinl <96008766+ANGJustinl@users.noreply.github.com>
2024-06-13 21:40:43 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -10,7 +10,7 @@ from mikazuki.launch_utils import (base_dir_path, catch_exception,
 from mikazuki.log import log
 
 parser = argparse.ArgumentParser(description="GUI for stable diffusion training")
-parser.add_argument("--host", type=str, default="127.0.0.1")
+parser.add_argument("--host", type=str, default="0.0.0.0")
 parser.add_argument("--port", type=int, default=28000, help="Port to run the server on")
 parser.add_argument("--listen", action="store_true")
 parser.add_argument("--skip-prepare-environment", action="store_true")
